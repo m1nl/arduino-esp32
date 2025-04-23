1685ef30f182428e0e76214083d108d123ee43b7
m1nl <mateusz@nalewajski.pl>
2025-04-23 13:49:57 +0200

    enable PM support in ESP-IDF

--- tools/platformio-build-esp32.py
+++ tools/platformio-build-esp32.py
@@ -336,7 +336,7 @@ env.Append(
         "UNITY_INCLUDE_CONFIG_H",
         "WITH_POSIX",
         "_GNU_SOURCE",
-        ("IDF_VER", '\\"v4.4.8-1-ga9d0f22193-dirty\\"'),
+        ("IDF_VER", '\\"v4.4.8\\"'),
         "ESP_PLATFORM",
         "_POSIX_READER_WRITER_LOCKS",
         "ARDUINO_ARCH_ESP32",
